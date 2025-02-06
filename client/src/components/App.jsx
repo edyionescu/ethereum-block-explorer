03791dfc5296214abc3850d1332dcf38af0b04d8
Edy Ionescu <errorsmith@gmail.com>
2025-02-06 16:15:35 +0200

    Add theme toggle component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Root from '../routes/Root';
 import Block from '../routes/Block';
 import Transaction from '../routes/Transaction';
+import ThemeToggle from './ThemeToggle';
 import Error from './Error';
 
 import { HOUR, MINUTE } from '../queries';
@@ -26,9 +27,14 @@ function App() {
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 mt-10">
-          <h1 className="ml-4 mb-6 text-3xl font-bold tracking-tight text-gray-900">
-            <Link to="/">Ethereum Block Explorer</Link>
-          </h1>
+          <div className="ml-4 mb-6 flex flex-wrap items-center justify-between sm:flex-nowrap">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <Link to="/">Ethereum Block Explorer</Link>
+            </h1>
+            <div className="flex-shrink-0 pointer-events-auto">
+              <ThemeToggle />
+            </div>
+          </div>
 
           <Routes>
             <Route path="/" element={<Root />} />
